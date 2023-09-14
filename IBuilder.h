1ff58669d363a370d51cde0c2d5af2aee1d5dd94
Tristan Baskerville <t.baskerville@digipen.edu>
2023-09-14 14:35:33 -0700

    Add Builder and DependencyList interfaces.

---
+++ IBuilder.h
@@ -0,0 +1,41 @@
+//>=------------------------------------------------------------------------=<//
+// file:	  IBuilder.h
+// author:	Tristan Baskerville
+// brief:	  Builder classes almost always use the same Build() implementation,
+//          this class defines a common interface to prioritize move operations.
+//
+// Copyright © 2022 DigiPen, All rights reserved.
+//>=------------------------------------------------------------------------=<//
+#ifndef IBUILDER_H
+#define IBUILDER_H
+
+namespace tbasque
+{
+  /////////////////////////////////////////////////////////////////////////////
+  /// Interface class for buildable objects. Provides a common function to 
+  /// output the object instance with a move operation.
+  ///
+  /// \tparam T Type of object being built.
+  /////////////////////////////////////////////////////////////////////////////
+  template <typename T>
+  class IBuilder
+  {
+  public:
+    ////////////////////////////////////////////////////////////////////////////
+    /// Returns the object instance using a move operation so that the 
+    /// object can avoid copies.
+    ///
+    /// \tparam T Type of object this Builder is constructing
+    ///
+    /// \returns  The instance stored in T as an r-value
+    ////////////////////////////////////////////////////////////////////////////
+    [[nodiscard]] T&& Build() 
+    {
+      return std::move(instance_); 
+    }
+  protected:
+    T instance_{};
+  };
+}
+
+#endif
---
+++ IDependencyList.h
@@ -0,0 +1,51 @@
+//>=------------------------------------------------------------------------=<//
+// file:	  IDependencyList.h
+// author:	Tristan Baskerville
+// brief:	  Provides dependent IServices as a Tuple of shared pointers.
+//          Constructing a dependent service before its dependency emits
+//          a compiler error.
+//
+// Copyright © 2022 DigiPen, All rights reserved.
+//>=------------------------------------------------------------------------=<//
+#ifndef IDEPENDENCY_LIST_H
+#define IDEPENDENCY_LIST_H
+
+namespace tbasque
+{
+  template <class... T>
+  class IDependencyList
+  {
+  public:
+    //  no modification to the tuple allowed after construction, so this enables
+    //  outside classes to know what kind of tuple to pass to the constructor
+    using Tuple = std::tuple<std::shared_ptr<T>...>;
+    //  shorthand so we don't have to pass around template params
+    using DependencyList = IDependencyList<T...>;
+    //  shorthand for getting the total NUMBER of dependencies
+    static constexpr uint DependencyCount = sizeof...(T);
+
+    IDependencyList(Tuple&& t) : pServices_(std::forward<Tuple>(t)) {}
+    IDependencyList(DependencyList&&) = default;
+
+  protected:
+    template <typename S>
+    std::shared_ptr<S> GetService()
+    {
+      //  Uses std::get to retrieve our desired system from the internal tuple
+      //  if the type S does not exist in the tuple, this does not compile.
+      return std::get<std::shared_ptr<S>>(pServices_);
+    }
+
+    template <typename S>
+    std::shared_ptr<S const> GetService() const
+    {
+      //  Same as above method but instead returned as a const shared pointer. 
+      return std::get<std::shared_ptr<S>>(pServices_);
+    }
+  private:
+    //  Stores pointers to dependent systems for GetService
+    Tuple pServices_;
+  };
+}
+
+#endif
