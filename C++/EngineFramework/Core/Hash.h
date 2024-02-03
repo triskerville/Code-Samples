6defb7df834e0961ef24289903289b9a9be10a21
Tristan Baskerville <t.baskerville@digipen.edu>
2024-02-02 21:40:48 -0800

    Update file contributors and add proper source to hash calculation.

--- C++/EngineFramework/Core/Hash.h
+++ C++/EngineFramework/Core/Hash.h
@@ -1,6 +1,7 @@
 // file:    Hash.h
-// author:  Tristan Baskerville
-// brief:   
+// author:  Tristan Baskerville, Gray Bangs
+// brief:   Provides the Hash class implementation, which creates a hash from a
+//          C-style string. 
 //
 // Copyright © 2021 DigiPen, All rights reserved.
 
@@ -10,11 +11,13 @@ namespace tbasque
 {
   class Hash 
   {
+    //  Author: Gray Bangs
     constexpr uint64_t calc_hash(const char* c) 
     {
       if (c == nullptr || *c == '\0')
         return 0;
 
+      //  Source:  https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
       constexpr uint64_t  base = 14695981039346656037;
       constexpr uint64_t prime = 1099511628211;
       uint64_t hash = base;
@@ -27,13 +30,11 @@ namespace tbasque
       return hash;
     }
 
-
     uint64_t val_;
 
   public:
     constexpr explicit Hash(const char* c) : val_(calc_hash(c)) {}
     constexpr Hash(uint64_t val) : val_(val) {}
     constexpr operator uint64_t() const { return val_; }
-
   };
 }
